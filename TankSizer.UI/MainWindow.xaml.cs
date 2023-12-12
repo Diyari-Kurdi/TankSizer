b9f77f758fe45199949e7902784785a172a70673
Diyari-Kurdi <dyari.ismaeil@outlook.com>
2023-12-12 14:21:05 +0300

    Adding Animation

--- TankSizer.UI/MainWindow.xaml.cs
+++ TankSizer.UI/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
 using TankSizer.UI.ViewModels;
 
 namespace TankSizer.UI;
@@ -12,5 +15,64 @@ public partial class MainWindow : Window
     {
         DataContext = new MainViewModel();
         InitializeComponent();
+    }
+
+    private void ItemBorder_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+    {
+        ColorAnimation colorAnimation = new()
+        {
+            From = Colors.WhiteSmoke,
+            To = Colors.LightGray,
+            BeginTime = TimeSpan.FromSeconds(.1),
+            Duration = TimeSpan.FromSeconds(.25)
+        };
+
+        ThicknessAnimation thicknessAnimation = new()
+        {
+            From = new Thickness(0),
+            To = new Thickness(15),
+            BeginTime = TimeSpan.FromSeconds(.1),
+            Duration = TimeSpan.FromSeconds(.25)
+        };
+
+        Storyboard.SetTarget(thicknessAnimation, (Border)sender);
+        Storyboard.SetTargetProperty(thicknessAnimation, new PropertyPath("Padding"));
+        Storyboard.SetTarget(colorAnimation, (Border)sender);
+        Storyboard.SetTargetProperty(colorAnimation, new PropertyPath("(Border.Background).(SolidColorBrush.Color)"));
+        Storyboard storyboard = new();
+        storyboard.Children.Add(thicknessAnimation);
+        storyboard.Children.Add(colorAnimation);
+
+        storyboard.Begin(this);
+    }
+
+    private void ItemBorder_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+    {
+        ColorAnimation colorAnimation = new()
+        {
+            From = Colors.LightGray,
+            To = Colors.WhiteSmoke,
+            BeginTime = TimeSpan.FromSeconds(.1),
+            Duration = TimeSpan.FromSeconds(.25)
+        };
+
+        ThicknessAnimation thicknessAnimation = new()
+        {
+            From = new Thickness(15),
+            To = new Thickness(0),
+
+            Duration = TimeSpan.FromSeconds(.25)
+        };
+
+        Storyboard.SetTarget(thicknessAnimation, (Border)sender);
+        Storyboard.SetTargetProperty(thicknessAnimation, new PropertyPath("Padding"));
+        Storyboard.SetTarget(colorAnimation, (Border)sender);
+        Storyboard.SetTargetProperty(colorAnimation, new PropertyPath("(Border.Background).(SolidColorBrush.Color)"));
+
+        Storyboard storyboard = new();
+        storyboard.Children.Add(thicknessAnimation);
+        storyboard.Children.Add(colorAnimation);
+
+        storyboard.Begin(this);
     }
 }
